091e2c5fda54b4fc96675d6362fdd23fd436c3aa
ansarifaheem1986 <ansarifaheemfnu@gmail.com>
2025-08-01 16:13:26 -0500

    rg_01_testing

--- azure_resource_group_create/01_rg/main.tf
+++ azure_resource_group_create/01_rg/main.tf
@@ -22,7 +22,7 @@ provider "azurerm" {
    
 }
 resource "azurerm_resource_group" "myrg" {
-  name = "DevOps1-prod-eastus-01-rg"
+  name = "DevOps-prod-eastus-01-rg"
   location = "eastus"
   #resource block to create the azure resource group*/
 }
