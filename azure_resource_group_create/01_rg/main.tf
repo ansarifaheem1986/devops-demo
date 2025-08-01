942a4f2e198b9d96fbe643b35947ad31d9eaf7ba
ansarifaheem1986 <ansarifaheemfnu@gmail.com>
2025-08-01 10:38:46 -0500

    Added the backend store for TFState

--- azure_resource_group_create/01_rg/main.tf
+++ azure_resource_group_create/01_rg/main.tf
@@ -8,11 +8,18 @@ terraform {
     version = ">= 4.25.0"
    }
   }
+    backend "azurerm" {
+    resource_group_name  = "terraform-storage-files-01-rg"
+    storage_account_name = "tfstatefilesdevprd"
+    container_name       = "githubaction"
+    key                  = "azure_rg_create_destroy/rg.terraform.state"
+  }
 }
 
 provider "azurerm" {
   features {}
   subscription_id = "d49174c3-680b-4508-a934-4fe38df8598f"
+   
 }
 resource "azurerm_resource_group" "myrg" {
   name = "DevOps-prod-eastus-01-rg"
