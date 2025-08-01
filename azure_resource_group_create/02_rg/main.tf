354340820987817cc7184ca67b4ce46d23ff2fab
ansarifaheem1986 <ansarifaheemfnu@gmail.com>
2025-08-01 12:44:51 -0500

    destroy 02

--- azure_resource_group_create/02_rg/main.tf
+++ azure_resource_group_create/02_rg/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "myrg" {
-  name = "DevOps-prod-eastus-02-rg"
+  name = "DevOps2-prod-eastus-02-rg"
   location = "eastus"
   #resource block to create the azure resource group
 }
