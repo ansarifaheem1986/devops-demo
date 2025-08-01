b147c648ccbadcfbf0d1b539ca6fde1714d353ef
ansarifaheem1986 <ansarifaheemfnu@gmail.com>
2025-08-01 15:52:37 -0500

    added the destroy3

---
+++ azure_resource_group_create/03_rg/variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  description = "The location of the resource group"
+  type = string
+  default = "eastus"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type = string
+  default = "DevOps3-eastus-03-rg"
+}
