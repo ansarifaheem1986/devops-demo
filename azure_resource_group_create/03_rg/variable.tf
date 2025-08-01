269698cdc3fadd4fa76bf3dca46766ce3f5e1efa
ansarifaheem1986 <ansarifaheemfnu@gmail.com>
2025-08-01 15:10:35 -0500

    Added the 03 rg

---
+++ azure_resource_group_create/03_rg/variable.tf
@@ -0,0 +1,10 @@
+variable "location" {
+  description = "The location of the resource group"
+  type = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type = string
+  default = "DevOps3-eastus-03-rg"
+}
